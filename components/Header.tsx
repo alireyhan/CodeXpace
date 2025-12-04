20278314df97df5e11f91f421edc28101b9663e5
AbdullahAafzal <abdullahafzal@aglivo.com>
2025-12-04 15:49:46 +0500

    new

--- components/Header.tsx
+++ components/Header.tsx
@@ -63,7 +63,6 @@ export default function Header() {
             <span className="text-white text-base font-bold">CodeXpace</span>
           </Link>
 
-          {/* Navigation Links */}
           <div className="hidden md:flex items-center space-x-6">
             <Link
               href="/"
